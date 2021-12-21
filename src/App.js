3c7a78e322ac21af334839be2b039e2bbda3c9e2
Sam'an <samangriffiths47@gmail.com>
2021-12-21 02:19:04 -0600

    finishing touches

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import Nav from './components/Nav'
 import Home from './pages/Home'
 import { LoadDiscoverMovies } from './store/actions/MovieActions'
 import { useEffect, useState } from 'react'
-// import MovieRoutes from './pages/movies/MovieRoutes'
 import DiscoverMovies from './pages/movies/DiscoverMovies'
 import DiscoverMoviesSelection from './components/movies/discover/DiscoverMoviesSelection'
 import { LoadDiscoverShows } from './store/actions/TVShowActions'
-// import TVRoutes from './pages/shows/TVRoutes'
 import DiscoverShows from './pages/shows/DiscoverShows'
 import DiscoverShowsSelection from './components/shows/discover/DiscoverShowsSelection'
 import { useHistory } from 'react-router-dom'
@@ -39,11 +37,6 @@ function App(props) {
       <Nav />
       <main>
         <Route exact path="/" render={(...props) => <Home {...props} />} />
-        {/* <Route
-          exact
-          path="/movies"
-          render={(...props) => <MovieRoutes {...props} />}
-        /> */}
         <Route
           path="/movies/discover"
           render={(...props) => <DiscoverMovies {...props} />}
@@ -58,11 +51,6 @@ function App(props) {
             )}
           />
         ))}
-        {/* <Route
-          exact
-          path="/shows"
-          render={(...props) => <TVRoutes {...props} />}
-        /> */}
         <Route
           path="/shows/discover"
           render={(...props) => <DiscoverShows {...props} />}
